d745dde550dbbef071d178fbc85e679e97508517
sudarshan604 <sudarshandn1999@gmail.com>
2023-05-23 10:44:28 +0545

    regexx start store api

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,23 +3,39 @@ const Product =require('../models/product')
 
 const getAllProductsStatic=async(req,res)=>{
   
-     const products=await Product.find({name:'a first wooden table'});
+     const search='ab'
      
-      res.status(200).json({products,
+     const products=await Product.find({name:{$regex:search,$options:'i'}});
+   
+     res.status(200).json({products,
           nbHits:products.length
-     
-     })
+      })
      // res.status(200).json({msg:'product testing route'})
 
 
 }
-
 const getAllProducts=async (req,res)=>{
    
-     const products=await Product.find(req.query)
+  const {featured,company,name}=req.query  //if from url page and featured pass as argument
+  
+  const queryObject={}
 
-    console.log(req.query)
-     res.status(200).json({products,nbHits:products.length})
+  if(featured){                  //if there is featured then it only retutn featured true or false
+      queryObject.featured=featured==='true'?true:false
+  }
+
+   if(company){
+       queryObject.company=company
+   }
+
+if(name){
+      queryObject.name={$regex:name,$options:'i'}
+}
+
+  console.log(queryObject)         //if queryObject is empty {} if we passed page also then also all the value will return
+   const products=await Product.find(queryObject)
+   res.status(200).json({products,nbHits:products.length})
+
 }
 
 module.exports={
--- 04-store-api/starter/populate.js
+++ 04-store-api/starter/populate.js
@@ -8,7 +8,7 @@ const jsonProduct=require('./products.json')
 
 const start=async ()=>{
      try {
-         await connectDB(process.env.MONGO_URI)
+          await connectDB(process.env.MONGO_URI)
           await Product.deleteMany();
           await Product.create(jsonProduct)
           console.log('success!!!')
